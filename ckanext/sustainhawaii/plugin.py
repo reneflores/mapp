92cf9c2c8396d41f93ae4cf393c198edbe9622ab
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-08-19 18:01:34 -0400

    compile and commit various static assets and CSS

--- ckanext/sustainhawaii/plugin.py
+++ ckanext/sustainhawaii/plugin.py
@@ -6,7 +6,9 @@ class HawaiiThemePlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
 
     def configure(self, config):
-        p.toolkit.add_resource('static', 'ckanext-hawaii')
+        #p.toolkit.add_resource('static', 'ckanext-sustainhawaii')
+        pass
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
+        p.toolkit.add_public_directory(config, 'static')
